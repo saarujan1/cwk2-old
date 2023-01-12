e5a2c8d1135efe99ac140a4a5065a62071779e72
sy2g19 <sy2g19@soton.ac.uk>
2023-01-12 08:57:19 +0000

    New functions in Matches and Discover

--- client/src/views/DiscoverView.tsx
+++ client/src/views/DiscoverView.tsx
@@ -1,5 +1,8 @@
-import React, { useState, createRef, useRef, useMemo } from 'react'
+import React, {useEffect, useState, createRef, useRef, useMemo } from 'react'
 import TinderCard from 'react-tinder-card'
+import { useAppContext } from '../store/UniContext'
+import { getAzure } from '../store/helpers'
+
 const rejectIcon = require('../assets/icons/reject.svg').default as string
 const undoIcon = require('../assets/icons/undo.svg').default as string
 const acceptIcon = require('../assets/icons/accept.svg').default as string
@@ -7,32 +10,59 @@ const acceptIcon = require('../assets/icons/accept.svg').default as string
 const characters = [
   {
     name: 'Cristiano Ronaldo',
-    url: '',
+    email:'',
+    phone: '',
+    bio: '',
+    hobbies: '',
+    age: '',
   },
   {
     name: 'Lionel Messi',
-    url: '',
   },
   {
     name: 'Neymar',
-    url: '',
   },
   {
     name: 'David Beckham',
-    url: '',
   },
   {
     name: 'Harry Maguire',
-    url: '',
   },
 ]
 
 export default function DiscoverView() {
-  const [data, setData] = React.useState('')
-  const [login, setLogin] = React.useState('')
   const [currentIndex, setCurrentIndex] = useState(characters.length - 1)
-  const [lastDirection, setLastDirection] = useState()
   const currentIndexRef = useRef(currentIndex)
+  const [globalState, dispatch] = useAppContext()
+  const [user, setUser] = useState(null)
+  const [lastDirection, setLastDirection] = useState()
+
+
+  /**
+   * TO DO FOR DISCOVER VIEW:
+   * RIGHT SWIPE --> acceptUsers
+   * LEFT SWIPE --> rejectedUsers
+   * Get rid of Go Back
+   * Accepted users to show on Matches page
+   */
+
+  const getUser = async () => {
+    try {
+      let promise = new Promise((resolve, reject) => getAzure(resolve, '/api/updateAccount?', {username: globalState.user.id, password: globalState.password}))
+      let resp = (await promise) as any
+      if(resp.account){
+        setUser(resp.account)
+      }
+    } catch (error) {
+        console.log(error)
+      }
+  }
+
+  useEffect(() => {
+    getUser()
+  }, [])
+
+  // console.log('user', user)
 
   const childRefs = useMemo(
     () =>
@@ -50,11 +80,26 @@ export default function DiscoverView() {
   const canGoBack = currentIndex < characters.length - 1
   const canSwipe = currentIndex >= 0
 
-  const swiped = (direction, nameToDelete, index) => {
-    setLastDirection(direction)
-    updateCurrentIndex(index - 1)
+  const updateMatches = async (matchedUserId) => {
+    try {
+      let promise = new Promise((resolve, reject) => getAzure(resolve, '/api/updateAccount?', {username: globalState.user.id, password: globalState.password}))
+      let resp = (await promise) as any
+      if(resp.account){
+        setUser(resp.account)
+      }
+    } catch (error) {
+        console.log(error)
+      }
   }
 
+  console.log(user)
+
+  const swiped = (direction, nameToDelete, swipedUserId) => {
+    if (direction === 'right') {
+         updateMatches(swipedUserId)
+    }
+     setLastDirection(direction)
+}
   const swipe = async (dir) => {
     if (canSwipe && currentIndex < characters.length) {
       await childRefs[currentIndex].current.swipe(dir) // Swipe the card!
@@ -85,8 +130,8 @@ export default function DiscoverView() {
       <div className="card-container">
         {characters.map((character, index) => (
           <TinderCard ref={childRefs[index]} className="swipe" key={character.name} onSwipe={(dir) => swiped(dir, character.name, index)} onCardLeftScreen={() => outOfFrame(character.name, index)}>
-            <div style={{ backgroundImage: 'url(' + character.url + ')' }} className="card">
-              <h3>{character.name}</h3>
+            <div className="card">
+              <h3 className="">{character.name}</h3>
             </div>
           </TinderCard>
         ))}
--- client/src/views/MatchesView.tsx
+++ client/src/views/MatchesView.tsx
@@ -1,4 +1,13 @@
+import { useAppContext } from '../store/UniContext'
+import { getAzure } from '../store/helpers'
+import { useState } from 'react'
+
 export default function MatchesView() {
+
+  /*Needs to display all matches for given user
+   * This should be in the form of a label
+   */
+  
   return (
     <div>
       <h1 className="pageTitle"> Matches</h1>
