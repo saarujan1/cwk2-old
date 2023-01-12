21af57aa8750319296899c1adbbb65cb8e07d668
Patricia <pww1u19@soton.ac.uk>
2023-01-12 14:59:40 +0100

    fix: merge changes in discoverview

--- client/src/views/DiscoverView.tsx
+++ client/src/views/DiscoverView.tsx
@@ -94,85 +94,30 @@ export default function DiscoverView() {
 
   return (
     <>
-      {/* <div className="d-flex flex-column h-100 align-items-center">
-        <Panel
-          height="h70"
-          width="col-12"
-          padding={3}
-          className="d-flex justify-content-center"
-        >
+      <div className="d-flex flex-column h-100 align-items-center">
+        <h1 className="pageTitle"> Discover</h1>
+        <Panel height="h70" width="col-12" padding={3} className="d-flex justify-content-center">
           <div className="card-container">
             {characters.map((character, index) => (
-              <TinderCard
-                ref={childRefs[index]}
-                className="swipe"
-                key={character.name}
-                onSwipe={(dir) => swiped(dir, character.name, index)}
-                onCardLeftScreen={() => outOfFrame(character.name, index)}
-              >
+              <TinderCard className="swipe" key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
                 <div className="card">
                   <h3>{character.name}</h3>
+                  <div className="attributes">
+                    <p className="attribute-text"> {character.email}</p>
+                    <p className="attribute-text"> {character.bio}</p>
+                    <p className="attribute-text"> {character.hobbies}</p>
+                    <p className="attribute-text"> {character.age}</p>
+                  </div>
                 </div>
               </TinderCard>
             ))}
           </div>
         </Panel>
-        <Panel
-          height="h30"
-          width="col-12"
-          padding={3}
-          className="d-flex justify-content-center"
-        >
-          <img
-            onClick={() => swipe("left")}
-            role="button"
-            src={rejectIcon}
-            alt={"Reject"}
-            width="100"
-            height="100"
-            aria-label={"Reject"}
-          />
-          <img
-            onClick={() => goBack()}
-            role="button"
-            src={undoIcon}
-            alt={"Undo"}
-            width="100"
-            height="100"
-            aria-label={"Undo"}
-          />
-          <img
-            onClick={() => swipe("right")}
-            role="button"
-            src={acceptIcon}
-            alt={"Accept"}
-            width="100"
-            height="100"
-            aria-label={"Accept"}
-          />
+        <Panel height="h30" width="col-12" padding={3} className="d-flex justify-content-center">
+          <img onClick={() => swiped('left', 'Cristiano Ronaldo')} role="button" src={rejectIcon} alt={'Reject'} width="100" height="100" aria-label={'Reject'} />
+          {/* <img onClick={() => goBack()} role="button" src={undoIcon} alt={'Undo'} width="100" height="100" aria-label={'Undo'} /> */}
+          <img onClick={() => swiped('right', 'Harry Maguire')} role="button" src={acceptIcon} alt={'Accept'} width="100" height="100" aria-label={'Accept'} />
         </Panel>
-      </div> */}
-
-      <h1 className="pageTitle"> Discover</h1>
-      <h3> Who would you like to match with today? </h3>
-      <div className="card-container">
-        {characters.map((character, index) => (
-          <TinderCard className="swipe" key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
-            <div className="card">
-              <h3>{character.name}</h3>
-              <div className="attributes">
-                <p className="attribute-text"> {character.email}</p>
-                <p className="attribute-text"> {character.bio}</p>
-                <p className="attribute-text"> {character.hobbies}</p>
-                <p className="attribute-text"> {character.age}</p>
-              </div>
-            </div>
-          </TinderCard>
-        ))}
-      </div>
-      <div className="buttons">
-        <img onClick={() => swiped('left', 'Cristiano Ronaldo')} role="button" src={rejectIcon} alt={'Reject'} width="70" height="70" aria-label={'Reject'} />
-        <img onClick={() => swiped('right', 'Harry Maguire')} role="button" src={acceptIcon} alt={'Accept'} width="70" height="70" aria-label={'Accept'} />
       </div>
     </>
   )
